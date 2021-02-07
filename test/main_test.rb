709deec02338ae412b1ac73113d26184715ef15e
Kornelia <korneliacieslik08@gmail.com>
2021-02-07 19:49:11 +0100

    Improve tests for Counter

--- test/main_test.rb
+++ test/main_test.rb
@@ -15,20 +15,23 @@ class MainTest < Minitest::Test
 
   def test_views_for_ip
     log = Log.new(File.readlines('webserver_test.log'))
-    hash = views_for_ip(log.data)
-    assert hash["/help_page/1 126.318.035.038\n"] == 2 
+    catalog = views_for_ip(log.data)
+    assert catalog["/help_page/1 126.318.035.038\n"] == 2
+    assert catalog.length == 4 
   end 
 
   def test_unique_views
     log = Log.new(File.readlines('webserver_test.log'))
-    hash = unique_views(views_for_ip(log.data))
-    assert hash["/help_page/1"] == 3
+    catalog = unique_views(views_for_ip(log.data))
+    assert catalog["/help_page/1"] == 3
+    assert catalog.length == 2
   end 
 
   def test_total_views
     log = Log.new(File.readlines('webserver_test.log'))
-    hash = total_views(views_for_ip(log.data))
-    assert hash["/help_page/1"] == 4
+    catalog = total_views(views_for_ip(log.data))
+    assert catalog["/help_page/1"] == 4
+    assert catalog.length == 2
   end
 
 end 
