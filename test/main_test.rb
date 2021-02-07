7e0668a1403812be2e9edeefb0d70f2ef0ba20c1
Kornelia <korneliacieslik08@gmail.com>
2021-02-07 20:46:27 +0100

    Improve tests. Add minitest-reporters gem.

--- test/main_test.rb
+++ test/main_test.rb
@@ -5,33 +5,33 @@ class MainTest < Minitest::Test
 
   def test_data_presence
     log = Log.new(File.readlines('webserver_test.log'))
-    assert log.data != nil 
+    refute_equal log.data, nil 
   end 
 
   def test_data_content_presence 
     log = Log.new(File.readlines('webserver_test.log'))
-    assert log.data[0] == "/help_page/1 126.318.035.038\n"
+    assert_equal log.data[0], "/help_page/1 126.318.035.038\n"
   end 
 
   def test_views_for_ip
     log = Log.new(File.readlines('webserver_test.log'))
     catalog = views_for_ip(log.data)
-    assert catalog["/help_page/1 126.318.035.038\n"] == 2
-    assert catalog.length == 4 
+    assert_equal catalog["/help_page/1 126.318.035.038\n"], 2
+    assert_equal catalog.length, 4 
   end 
 
   def test_unique_views
     log = Log.new(File.readlines('webserver_test.log'))
     catalog = unique_views(views_for_ip(log.data))
-    assert catalog["/help_page/1"] == 3
-    assert catalog.length == 2
+    assert_equal catalog["/help_page/1"], 3
+    assert_equal catalog.length, 2
   end 
 
   def test_total_views
     log = Log.new(File.readlines('webserver_test.log'))
     catalog = total_views(views_for_ip(log.data))
-    assert catalog["/help_page/1"] == 4
-    assert catalog.length == 2
+    assert_equal catalog["/help_page/1"], 4
+    assert_equal catalog.length, 2
   end
 
   def test_total_views_list
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,4 @@
 require_relative '../main'
 require 'minitest/autorun'
+require 'minitest/reporters'
+Minitest::Reporters.use!
